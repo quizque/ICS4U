fc5274c282225750ebad95cfe1f027027f4089ad
Nick <nickcoombe@outlook.com>
2020-03-12 09:54:19 -0400

    Updated ISU

---
+++ ISU/src/DateSimulator.py
@@ -0,0 +1,44 @@
+import pygame
+import config
+from elements import actor
+ 
+pygame.init()
+
+screen = pygame.display.set_mode(config.SCREEN_SIZE)
+ 
+pygame.display.set_caption("Dating Simulator")
+
+
+done = False
+
+clock = pygame.time.Clock()
+
+all_sprites_list = pygame.sprite.Group()
+
+#f = pygame.image.load("p.png").convert()
+
+
+
+mainAct = actor.Actor((500,500), 0.5, ["p.png"])
+
+all_sprites_list.add(mainAct)
+
+while not done:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    
+
+    screen.fill((0,25,0))
+
+    all_sprites_list.draw(screen)
+    
+
+    pygame.display.flip()
+    
+    clock.tick(60)
+ 
+
+pygame.quit()
--- ISU/src/Elements/Actor.py
+++ ISU/src/Elements/Actor.py
@@ -1,12 +1,20 @@
 import pygame
 
+# Actor expression constants
 EXPRESSION_DEFAULT = 0
 EXPRESSION_HAPPY   = 1
 EXPRESSION_UPSET   = 2
 
-class Actor():
+
+# Actor class
+class Actor(pygame.sprite.Sprite):
 
     def __init__(self, _position, _scale, _expression_list):
+        
+        # Initialize the sprite class
+        super().__init__()
+        
+        # Define the position, scale, and rotation of the actor
         self.position = _position
         self.scale = _scale
         self.expression_list = []
@@ -14,7 +22,7 @@ class Actor():
         for image_path in _expression_list:
             tmp_img = pygame.image.load(image_path)
             
-            self.expression_list.append(pygame.transform.scale(tmp_img.convert(), (tmp_img.get_width(), tmp_img.get_height())))
+            self.expression_list.append(pygame.transform.scale(tmp_img.convert_alpha(), (int(tmp_img.get_width()*self.scale), int(tmp_img.get_height()*self.scale))))
 
-    def draw(self, screen):
-        ...
+        self.image = self.expression_list[EXPRESSION_DEFAULT]
+        self.rect = self.image.get_rect()
