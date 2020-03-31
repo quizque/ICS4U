fb015bb8cbf65c9b572ab652cc49bf1c8ef0e1be
Nick <nickcoombe@outlook.com>
2020-03-31 19:58:11 -0400

    Added PID System

--- Labs/Lab 8/Animation.py
+++ Labs/Lab 8/Animation.py
@@ -24,31 +24,65 @@ def clamp(value, min, max):
     else:
         return value
 
+class PID():
+    def __init__(self, p, i, d):
+        self.p = p
+        self.i = i
+        self.d = d
+        self.prev_error = 0
+        self.prev_value = 0
+        self.prev_time = pygame.time.get_ticks()
+
+    def p_c(self, error):
+        return self.p * error
+
+    def i_c(self, error):
+        return 0
+
+    def d_c(self, error):
+        return self.d * (error/(pygame.time.get_ticks()- self.prev_time))
+    
+    def calc(self, input):
+        error = input - self.prev_value
+        value = self.p_c(error) + self.i_c(error) + self.d_c(error-self.prev_error)
+        print(error, value)
+
+        self.prev_value = value
+        self.prev_error = error
+        self.prev_time = pygame.time.get_ticks()
+        return value
+
 class Slime():
     def __init__(self, img, pos, min, max):
         self.img = img
         self.pos = pos
         self.min = min
         self.max = max
+        self.pid = PID(0.0075,0,2.25)
 
         self.v = (0,0)
         self.a = (0,20)
     
     def update(self):
-        
+
         self.v = (self.a[0] + self.v[0], self.a[1] + self.v[1])
-        print(clamp(self.v[1],0,300))
+
+
+        # FOLLOW MOUSE
+        t = self.pid.calc(pygame.mouse.get_pos()[0]-self.pos[0])
+        self.v = (self.v[0] + t, self.v[1])
+
         # 0 -> 300
-        self.v = (self.v[0],clamp(self.v[1],-10,10))
-        
-        self.pos = (self.pos[0] + self.v[0], clamp(self.pos[1] + self.v[1], 0, 263))0
+        self.v = (clamp(self.v[0],-10, 10),clamp(self.v[1],-10,10))
+        self.pos = (clamp(self.pos[0] + self.v[0], self.min, self.max), clamp(self.pos[1] + self.v[1], 0, 263))
 
     def draw(self):
         global screen
         
-        screen.blit(self.img, self.pos)
+        screen.blit(self.img, (self.pos[0]-37, self.pos[1]))
 
-green_slime = Slime(img_slime_green, (0,0), 0, 300)
+green_slime = Slime(img_slime_green, (0,262), 37, 334)
+red_slime = Slime(img_slime_red, (560,262), 413, 712)
 
 while not done:
     for event in pygame.event.get():
@@ -62,9 +96,11 @@ while not done:
 
     # Update Elements
     green_slime.update()
+    red_slime.update()
 
     # Draw elements
     green_slime.draw()
+    red_slime.draw()
 
     pygame.display.flip()
 
